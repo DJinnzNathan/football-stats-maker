112535a9b60fc8b1794872dd718ce2df643aa641
D-Jinnz Nathan <djinnz.nathan@gmail.com>
2021-09-10 03:10:46 +0200

    time in header, added key support, bg color toggle

--- src/script.js
+++ src/script.js
@@ -1,5 +1,8 @@
-var time, sec = 0, min = 0, active = false;
-var possessionTotal = 0, possessionHome = 0;
+let root = document.documentElement;
+let time, sec = 0, min = 0, active = false;
+let possessionTotal = 0, possessionHome = 0;
+let shotsHomeTotal = 0, shotsHome = 0;
+let shotsAwayTotal = 0, shotsAway = 0;
 
 function startTimer() {
     time = setTimeout(add, 1000)
@@ -23,13 +26,12 @@ function add() {
 
 function printData() {
     printTime();
-    printOnTitle();
     printPoss();
 }
 
 function printTime() {
-    document.getElementById('min').innerText = min;
-    document.getElementById('sec').innerText = ((sec < 10) ? '0' + sec : sec);
+    document.title = min + ':' + ((sec < 10) ? '0' + sec : sec);
+    document.getElementById('heading').innerText = min + ':' + ((sec < 10) ? '0' + sec : sec);
 }
 
 function toggleTimer() {
@@ -39,6 +41,7 @@ function toggleTimer() {
     } else {
         stopTimer();
         active = false;
+        resetBG();
     }
 }
 
@@ -54,6 +57,17 @@ function toggleIconStatus() {
         document.getElementById('gameStatus').classList.remove("spin");
     } else {
         document.getElementById('gameStatus').classList.add("spin");
+    }
+}
+
+function togglePossession() {
+    var poss = document.getElementById('possessionStatus').checked;
+    if (poss) {
+        document.getElementById('possessionStatus').checked = false;
+        if (active) document.body.style.backgroundColor = 'var(--home)';
+    } else {
+        document.getElementById('possessionStatus').checked = true;
+        if (active) document.body.style.backgroundColor = 'var(--away)';
     }
 }
 
@@ -86,12 +100,60 @@ function printPoss() {
     console.log('Possession: HOME ' + homePoss + '% - ' + awayPoss + '% AWAY');
 }
 
-function printOnTitle() {
-    document.title = min + ':' + ((sec < 10) ? '0' + sec : sec);
-}
-
 function setupSecondLeg() {
     sec = 0;
     min = 45;
     printTime();
+}
+
+function addShot(isHome, isOnGoal) {
+    if (isHome) {
+        shotsHomeTotal++;
+        if (isOnGoal) shotsHome++;
+    } else {
+        shotsAwayTotal++;
+        if (isOnGoal) shotsAway++;
+    }
+    console.log('Shots: ');
+    console.log('HOME ' + shotsHomeTotal + ' (' + shotsHome + ')');
+    console.log('AWAY ' + shotsAwayTotal + ' (' + shotsAway + ')');
+    printShots();
+}
+
+function printShots() {
+    document.getElementById('shotsTotalHome').innerText = shotsHomeTotal;
+    document.getElementById('shotsTotalAway').innerText = shotsAwayTotal;
+
+    document.getElementById('shotsTotalBar').value = (shotsHomeTotal / (shotsHomeTotal + shotsAwayTotal)) * 100;
+
+    document.getElementById('shotsHome').innerText = shotsHome;
+    document.getElementById('shotsAway').innerText = shotsAway;
+
+    document.getElementById('shotsBar').value = (shotsHome / (shotsHome + shotsAway)) * 100;
+}
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'h') {
+        document.getElementById('possessionStatus').checked = false;
+    }
+    if (event.key === 'a') {
+        document.getElementById('possessionStatus').checked = true;
+    }
+    if (event.key === 'r') {
+        resetBG();
+    }
+    if (event.key === 'p') {
+        toggleGame();
+    }
+    if (event.key === 't') {
+        togglePossession();
+    }
+});
+
+function changeColor(obj) {
+    root.style.setProperty('--away', '#' + obj.value);
+}
+
+function resetBG() {
+    document.body.style.backgroundColor = 'var(--bg)';
 }
